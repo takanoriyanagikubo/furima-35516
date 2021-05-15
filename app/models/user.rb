d02c8d84c9ee2de646fabc2d150fd7d04d611775
takanoriyanagikubo <82752800+takanoriyanagikubo@users.noreply.github.com>
2021-05-15 18:25:51 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,17 @@ class User < ApplicationRecord
          validates :password,null: false, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers"}
         
          with_options presence: true do
-          validates :nick_name, null: false 
-          validates :email, null: false 
-          validates :first_name, null: false, format: { with: /\A[ぁ-んァ-ン一-龥々ー]/} 
-          validates :last_name, null: false, format: { with: /\A[ぁ-んァ-ン一-龥々ー]/} 
-          validates :first_name_kana, null: false, format: { with: /\A[ァ-ヶー－]+\z/} 
-          validates :last_name_kana, null: false, format: { with: /\A[ァ-ヶー－]+\z/} 
-          validates :birthday, null: false 
+          validates :nick_name
+          validates :email
+          validates :birthday
+          with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]/}  do
+          validates :first_name
+          validates :last_name
+          end
+          with_options format: { with: /\A[ァ-ヶー－]+\z/}  do
+          validates :first_name_kana
+          validates :last_name_kana
+          end
+          
         end
 end
