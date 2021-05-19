29266efe59014b8285d93114ea2160973dc1767d
takanoriyanagikubo <82752800+takanoriyanagikubo@users.noreply.github.com>
2021-05-19 16:02:40 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,7 @@ class ItemsController < ApplicationController
     if @item.destroy
       redirect_to root_path
     else
-      redirect_to root_path
+      redirect_to edit_item_path
     end
   end
 
