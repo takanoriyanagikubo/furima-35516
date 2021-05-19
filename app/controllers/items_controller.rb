3a8e5f7f0e9b6cb1bb734ce328f3e87cf34cf1fb
takanoriyanagikubo <82752800+takanoriyanagikubo@users.noreply.github.com>
2021-05-19 14:44:52 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
   
 
@@ -36,6 +36,16 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+
   end
 
   def move_to_index
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "items#index"
-  resources :items, only: [ :create, :new, :show, :edit, :update]
+  resources :items, only: [ :create, :new, :show, :edit, :update, :destroy]
 
 end
 
