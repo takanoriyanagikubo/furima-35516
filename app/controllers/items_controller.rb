366493e16766361d074a1b8b894e21957d11e9b7
takanoriyanagikubo <82752800+takanoriyanagikubo@users.noreply.github.com>
2021-05-23 21:44:22 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -56,7 +56,7 @@ class ItemsController < ApplicationController
   end
 
   def edit_root
-    if  @item.buy.present?
+    if @item.user_id == current_user.id && @item.buy.present?
       redirect_to action: :index
     end
 end
